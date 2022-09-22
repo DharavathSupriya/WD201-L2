cd313395b027ccf24020d7ecf10cdb53adbfe560
DharavathSupriya <96606091+DharavathSupriya@users.noreply.github.com>
2022-09-22 19:38:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 const http = require("http");
 const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
 fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-  });
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) { 
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port);
